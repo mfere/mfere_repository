03046252396cb542ab3d33243f98c45171c081c4
Chan, Brian | Brian | GCSD <brian.chan@rakuten.com>
2018-01-08 02:07:54 +0900

    update indicatorFactory

--- src/main/java/com/analyzer/constants/TrainingValue.java
+++ src/main/java/com/analyzer/constants/TrainingValue.java
@@ -9,24 +9,7 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum TrainingValue {
-    CANDLE_PATTERN_EXIST(1),
-
-    CLOSE_ABOVE(1),
-    CLOSE_BELOW(-1),
-
-    UPWARD(1),
-    DOWNWARD(-1),
-
-    POSITIVE(1),
-    NEGATIVE(-1),
-
-    OVER_SOLD(1),
-    OVER_BROUGHT(-1),
-
-    EXPANDING(1),
-    CONTRACTING(-1),
-
-    NO_SIGNAL(0);
-
+    INDICATOR_EXIST(1),
+    INDICATOR_NOT_EXIST(0);
     double value;
 }
